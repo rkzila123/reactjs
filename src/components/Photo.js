8cab9498b4f637bb6df6768cc5df295042140512
rohitk834927 <45118204+rohitk834927@users.noreply.github.com>
2019-01-25 15:26:42 +0530

    change in photo.js
    Added img tag

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,12 +1,11 @@
 import React ,{Component} from 'react'
-import GLImage from "gl-react-image";
 
 class Photo extends Component{
     render(){
 
         const post=this.props.post
         return <figure className="figure">
-        <GLImage scr={post.imagelink} alt={post.caption} />
+        <img scr={post.imagelink} alt={post.caption} />
         </figure> 
     }
 }
